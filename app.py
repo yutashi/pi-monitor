fe04f03f09afcc8a9716de5e7b71a7c3fb88aaff
yuta <katayama@xshell.io>
2018-03-26 17:51:47 +0900

    Change data as dict

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import datetime
 
 while True:
     # Get CPU percent
-    data = psutil.cpu_percent()
+    stat = psutil.cpu_percent()
 
     # Get disk usage
-    # data = psutil.disk_partitions()
+    # stat = psutil.disk_partitions()
 
     # Get users who are logging in the terminal
-    # data = psutil.users()
+    # stat = psutil.users()
 
     timestamp = datetime.datetime.now().strftime('%H:%M:%S.%f')
-    print("Data: %s, Timestamp: %s" % (data, timestamp))
+    data = {"timestamp": timestamp, "stat": stat}
+    print(data)
+
     time.sleep(30)
